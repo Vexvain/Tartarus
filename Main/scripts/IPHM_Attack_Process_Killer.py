ead3bdeb2c544be9b55a3228c8d221e2f2045f6b
Vexvain <60254325+Vexvain@users.noreply.github.com>
2020-09-21 20:22:17 -0400

    Create IPHM_Attack_Process_Killer.py

---
+++ Main/scripts/IPHM_Attack_Process_Killer.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+import subprocess, time
+
+def killer(cmd):
+    subprocess.call(cmd, shell=True)
+
+orange = "\x1b[38;5;202m"
+pink = "\x1b[38;5;201m"
+purple = "\x1b[38;5;93m"
+
+killer("pkill ldap; pkill ntp; pkill tftp; pkill ssdp; pkill portmap; pkill chargen; pkill netbios; pkill sentinel; pkill mssql; pkill ts3")
+killer("pkill HUN-FUN; pkill ASS-CRK; pkill EFT-PWR; pkill PMP-PMP; pkill VEX-VAIN")
+killer("pkill tcpabuse; pkill udpabuse; pkill dominate; pkill winsyn; pkill winseqid; pkill yubina; pkill prowin; pkill essyn; pkill csyn; pkill xsyn; pkill zsyn; pkill issyn; pkill nfobypass; pkill CF_Bypass; pkill ovhbypass; pkill bo4bypass")
+
+print(""+ pink +"╔═══════════════════════╗╔════════════════════╗ ╔════════════════╗")
+print(""+ pink +"║ "+ orange +"Killed all processes "+ pink +" ║║"+ orange +" vse "+ pink +"{"+ purple +"IPHM"+ pink +"}         "+ pink +"║ ║"+ orange +"    Arceus I    "+ pink +"║")
+print(""+ pink +"╠═══════════════════════╣║"+ orange +" telnet "+ pink +"{"+ purple +"IPHM"+ pink +"}      "+ pink +"║ ║"+ orange +" Process Killer "+ pink +"║")
+print(""+ pink +"║ "+ orange +"ldap "+ pink +"{"+ purple +"IPHM"+ pink +"}           "+ pink +"║║"+ orange +" tcpabuse "+ pink +"{"+ purple +"IPHM"+ pink +"}    "+ pink +"║ ║"+ orange +" Developer"+ pink +":"+ purple +" Vex "+ pink +"║")
+print(""+ pink +"║ "+ orange +"ntp "+ pink +"{"+ purple +"IPHM"+ pink +"}            "+ pink +"║║"+ orange +" udpabuse "+ pink +"{"+ purple +"IPHM"+ pink +"}    "+ pink +"║ ╚════════════════╝")
+print(""+ pink +"║ "+ orange +"tftp "+ pink +"{"+ purple +"IPHM"+ pink +"}           "+ pink +"║║"+ orange +" dominate "+ pink +"{"+ purple +"IPHM"+ pink +"}    "+ pink +"║")
+print(""+ pink +"║ "+ orange +"ssdp "+ pink +"{"+ purple +"IPHM"+ pink +"}           "+ pink +"║║"+ orange +" winsyn "+ pink +"{"+ purple +"IPHM"+ pink +"}      "+ pink +"║")
+print(""+ pink +"║ "+ orange +"portmap "+ pink +"{"+ purple +"IPHM"+ pink +"}        "+ pink +"║║"+ orange +" winseqid "+ pink +"{"+ purple +"IPHM"+ pink +"}    "+ pink +"║")
+print(""+ pink +"║ "+ orange +"chargen "+ pink +"{"+ purple +"IPHM"+ pink +"}        "+ pink +"║║"+ orange +" yubina "+ pink +"{"+ purple +"IPHM"+ pink +"}      "+ pink +"║")
+print(""+ pink +"║ "+ orange +"sentinel "+ pink +"{"+ purple +"IPHM"+ pink +"}       "+ pink +"║║"+ orange +" prowin "+ pink +"{"+ purple +"IPHM"+ pink +"}      "+ pink +"║")
+print(""+ pink +"║ "+ orange +"netbios "+ pink +"{"+ purple +"IPHM"+ pink +"}        "+ pink +"║║"+ orange +" essyn "+ pink +"{"+ purple +"IPHM"+ pink +"}       "+ pink +"║")
+print(""+ pink +"║ "+ orange +"mssql "+ pink +"{"+ purple +"IPHM"+ pink +"}          "+ pink +"║║"+ orange +" csyn "+ pink +"{"+ purple +"IPHM"+ pink +"}        "+ pink +"║")
+print(""+ pink +"║ "+ orange +"ts3 "+ pink +"{"+ purple +"IPHM"+ pink +"}            "+ pink +"║║"+ orange +" xsyn "+ pink +"{"+ purple +"IPHM"+ pink +"}        "+ pink +"║")
+print(""+ pink +"║ "+ orange +"HUN-FUN "+ pink +"{"+ purple +"IPHM"+ pink +"}        "+ pink +"║║"+ orange +" zsyn "+ pink +"{"+ purple +"IPHM"+ pink +"}        "+ pink +"║")
+print(""+ pink +"║ "+ orange +"ASS-CRK "+ pink +"{"+ purple +"IPHM"+ pink +"}        "+ pink +"║║"+ orange +" issyn "+ pink +"{"+ purple +"IPHM"+ pink +"}       "+ pink +"║")
+print(""+ pink +"║ "+ orange +"EFT-PWR "+ pink +"{"+ purple +"IPHM"+ pink +"}        "+ pink +"║║"+ orange +" ovhbypass "+ pink +"{"+ purple +"IPHM"+ pink +"}   "+ pink +"║")
+print(""+ pink +"║ "+ orange +"PMP-PMP "+ pink +"{"+ purple +"IPHM"+ pink +"}        "+ pink +"║║"+ orange +" cfbypass "+ pink +"{"+ purple +"IPHM"+ pink +"}    "+ pink +"║")
+print(""+ pink +"║ "+ orange +"VEX-VAIN "+ pink +"{"+ purple +"IPHM"+ pink +"}        "+ pink +"║║"+ orange +" nfobypass "+ pink +"{"+ purple +"IPHM"+ pink +"}   "+ pink +"║")
+print(""+ pink +"║ "+ orange +"Gang <3               "+ pink +"║║"+ orange +" bo4bypass "+ pink +"{"+ purple +"IPHM"+ pink +"}   "+ pink +"║")
+print(""+ pink +"╚═══════════════════════╝╚════════════════════╝")
+# Normal Menu < No Color Codes
+#╔═══════════════════════╗╔════════════════════╗ ╔══════════════════╗
+#║ Killed all processes  ║║ vse {IPHM}         ║ ║     Tartarus     ║
+#╠═══════════════════════╣║ telnet {IPHM}      ║ ║   IPHM_Attack    ║
+#║ ldap {IPHM}           ║║ tcpabuse {IPHM}    ║ ║  Process_Killer  ║
+#║ ntp {IPHM}            ║║ udpabuse {IPHM}    ║ ║ Developer: Vex   ║ 
+#║ tftp {IPHM            ║║ dominate {IPHM}    ║ ╚══════════════════╝
+#║ ssdp {IPHM            ║║ winsyn {IPHM}      ║
+#║ portmap {IPHM}        ║║ winseqid {IPHM}    ║
+#║ chargen {IPHM}        ║║ yubina {IPHM}      ║
+#║ sentinel {IPHM}       ║║ prowin {IPHM}      ║
+#║ netbios {IPHM}        ║║ essyn {IPHM}       ║
+#║ mssql {IPHM}          ║║ csyn {IPHM}        ║
+#║ ts3 {IPHM}            ║║ xsyn {IPHM}        ║
+#║ HUN-FUN {IPHM}        ║║ zsyn {IPHM}        ║
+#║ ASS-CRK {IPHM}        ║║ issyn {IPHM}       ║
+#║ EFT-PWR {IPHM}        ║║ ovhbypass {IPHM}   ║
+#║ PMP-PMP {IPHM}        ║║ cfbypass {IPHM}    ║
+#║ VEX-VAIN {IPHM}       ║║ nfobypass {IPHM}   ║
+#║ Gang <3               ║║ bo4bypass {IPHM}   ║
+#╚═══════════════════════╝╚════════════════════╝
